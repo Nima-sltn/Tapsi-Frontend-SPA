48db4ae4e840b66104c5786deb9439b061180d42
Nima-sltn <nimaso8228@gmail.com>
2024-01-03 15:44:24 +0330

    services section added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".nav");
-toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("nav__expanded");
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".nav");
+// toggler.addEventListener("click", (e) => {
+//   navbar.classList.toggle("nav__expanded");
+// });
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContnt = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+    //REMOVE PREVIOUS ACTIVED CLASS
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+    //ADD NEW ACTIVE CLASS
+    tab.classList.add("active");
+    targetTabContnt.classList.add("active");
+  });
 });
